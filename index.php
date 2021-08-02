5c83ca1b5fe9de41a1be058c8eefba8e266df1b3
Sahrul-ux <sahrulrajab11@gmail.com>
2021-08-02 20:35:58 +0700

    menambahkan fluid jumbotron

--- index.php
+++ index.php
@@ -38,6 +38,15 @@
     </nav>
     <!-- Akhir Navbar -->
 
+    <!-- jumbotron -->
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4">Fluid jumbotron</h1>
+            <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+        </div>
+    </div>
+    <!-- akhir jumbotron -->
+
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
