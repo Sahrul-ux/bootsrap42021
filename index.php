f698c4d8f114e7aef96a1607b532e723be10f6f0
Sahrul-ux <sahrulrajab11@gmail.com>
2021-08-02 19:54:16 +0700

    menambahkan link google foonts untuk memanggil fontsnya

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <!-- My Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet">
     <!-- My css -->
     <link rel="stylesheet" href="style.css">
     <title>Meow House</title>
